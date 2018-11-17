79da60d0453c17bb1d0a6f202d6638f46e913805
ssanmap <sebastian.smp21@gmail.com>
2018-11-16 21:14:14 -0300

    error -db stock

--- restalli/stock/admin.py
+++ restalli/stock/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Stock, ProductoStock, CategoriaStock
+
+admin.site.register(Stock)
+admin.site.register(ProductoStock)
+admin.site.register(CategoriaStock)
