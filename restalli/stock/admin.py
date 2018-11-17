d7982f4e9f1d91966131b8a2302bdf365ff6456b
ssanmap <sebastian.smp21@gmail.com>
2018-11-17 14:59:27 -0300

    prep master
    esteban

--- restalli/stock/admin.py
+++ restalli/stock/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import ProductoStock, CategoriaStock
 # Register your models here.
+
+admin.site.register(ProductoStock)
+admin.site.register(CategoriaStock)
