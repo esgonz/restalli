4b41ff511f6476f83d1013f3c6b2394ed3522237
Esteban Gonzalez <esteban.gonzalez@sqm.com>
2018-11-17 09:52:38 -0300

    stock admin
    panel stock admin funcionando

--- restalli/stock/admin.py
+++ restalli/stock/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CategoriaStock, Stock, ProductoStock
+
+admin.site.register(Stock)
+admin.site.register(CategoriaStock)
+admin.site.register(ProductoStock)
