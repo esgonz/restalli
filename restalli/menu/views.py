2a3a9fafa46bee986c9dee756b1ba4a3e6166370
Esteban Gonzalez <esteban.gonzalez@sqm.com>
2018-12-02 03:10:27 -0300

    stock y menu 90%
    stock y menu funcionando con casi todos los requerimientos.
    
    tablas con filtro de búsqueda, conexión entre tabla, actualización de
    selección storage , paginación, actualización, creacionismos.

--- restalli/menu/views.py
+++ restalli/menu/views.py
@@ -35,6 +35,8 @@ class MenuUpdate(generic.UpdateView):
 	model = ProductosMenu
 	form_class = ProductosMenuForm
 	success_url = reverse_lazy('menu:list')
+	template_name = 'menu/productosMenu_edit.html'
+
 
 class MenuDelete(generic.DeleteView):
 	model = ProductosMenu
@@ -48,7 +50,7 @@ class MenuList(generic.ListView):
 	def get_context_data(self, **kwargs):
 		# Call the base implementation first to get a context
 		context = super().get_context_data(**kwargs)
-		# Add in a QuerySet of all the books
+		# get all categories
 		context['categorias_list'] = CategoriaMenu.objects.all()
 		return context
 
@@ -135,33 +137,51 @@ class ProductosMenuStockList(generic.ListView):
 	paginate_by = 50
 
 
+	def get_queryset(self):
+		#leo el parametro que viene desde get(url)
+		filter_val = self.request.GET.get('categoria', '')
+		if filter_val!='':
+			#si el parametro existe, aplico el filtro.
+			return ProductoStock.objects.filter(categoria=filter_val)
+		else:
+			#si no, devuelvo todos los productos
+			return ProductoStock.objects.all()
+
+
 	def post(self, request, *args, **kwargs):
 		
-		print("POST ProductosMenuStockList")
+		print("-POST")
 
 		self.productoMenu_uuid = kwargs.get("producto")
+
+		#busco el producto/platillo que estoy actualizando
 		productoMenu = ProductosMenu.objects.get(uuid= kwargs.get("producto"))
 		print("PROD MENU")
 		print(productoMenu.uuid)
 		print(productoMenu.nombre)
 
 
-		#try to get cart, if cart doesnt exist, empty list
+		#selecciono la seleccion en cookies, si no creo 1 vacia
 		seleccion = self.request.session.get('seleccion', [])
 		# Do stuff with cart
 		request.session['seleccion'] = seleccion
 
-		
+		#controlando carro de seleccion
+		#
 		if 'clear' in request.POST:
+			print("*CLEAR")
 			request.session.flush()
-		elif 'add' in request.POST:
-			
+		
 
+		elif 'add' in request.POST or 'upde' in request.POST  :
+			print("*ADD")
+			#si se pulso "agregar" desde la lista de productos disponibles
 			uuid_producto = request.POST['uuid']
 			productoObject = ProductoStock.objects.get(uuid=uuid_producto)
 			qty_producto = request.POST['qty']
 
 			productoToAdd = {
+					"obj_uuid": None,
 					"uuid": str(productoObject.uuid),
 					"nombre": str(productoObject.nombre),
 					"qty":str(qty_producto)
@@ -169,52 +189,66 @@ class ProductosMenuStockList(generic.ListView):
 
 			temp_index = 0
 			
-		
+			#sumar qty si ya existe en el carro
 			for index, prod in enumerate(request.session['seleccion']):
 				print("PROD")
 				print(prod)
+				#si existe el id en el carro
 				if prod['uuid'] == uuid_producto:
 					#prev save object
 					producto_temp = prod
 					#then delete product
 					request.session['seleccion'].remove(prod)
 					
-					#change qty
-					temp_qty = int(producto_temp['qty']) + int(productoToAdd["qty"])
+					#change qty and add like a new brand
+					if 'add' in request.POST:
+						pass
+						temp_qty = int(producto_temp['qty']) + int(productoToAdd["qty"])
+					else:
+						temp_qty = int(productoToAdd["qty"])
+
 					productoToAdd["qty"] = temp_qty
 					break
 
+			#agregar producto al carro seleccion
 			request.session['seleccion'].append(productoToAdd)
 			print(request.session['seleccion'])
 		
 		elif 'save' in request.POST:
-			print("SAVE***")
+			
+			print("*SAVE")
 			for index, prod in enumerate(request.session['seleccion']):
 				
 				productoStock_aux = ProductoStock.objects.get(uuid= prod['uuid'])
 				print(productoStock_aux.uuid)
 				print(productoStock_aux.nombre)
-				productoMenu_to_save = ProductosMenuStock.objects.create(
-					productoStock_uuid = productoStock_aux,
-					productosMenu_uuid = productoMenu,
-					porciones = int(prod['qty']),
-				)
+				print(prod['obj_uuid'])
+
+				if prod['obj_uuid'] != None:
+					print("update product :")
+					print(productoStock_aux.nombre)
+					productoMenu_to_save = ProductosMenuStock.objects.get(uuid= prod['obj_uuid'])
+					productoMenu_to_save.porciones = int(prod['qty'])
+				else:
+					print("create product :")
+					print(productoStock_aux.nombre)
+					productoMenu_to_save = ProductosMenuStock.objects.create(
+						productoStock_uuid = productoStock_aux,
+						productosMenu_uuid = productoMenu,
+						porciones = int(prod['qty']),
+					)
 				#print(productoMenu_to_save)
-				#productoMenu_to_save.save()
+				productoMenu_to_save.save()
+
+		else:
+			request.session.flush()
 
 
-
-		return super().get(request, *args, **kwargs)
-
-
-	def get(self, request, *args, **kwargs):
-		#self.object = self.get_object(queryset=Publisher.objects.all())
-		self.productoMenu_uuid = kwargs.get("producto")
 		return super().get(request, *args, **kwargs)
 
 
 	def get_context_data(self, **kwargs):
-		print("NUEVO LIST CART:")
+		print("--CONTEXT DATA:")
 		# Call the base implementation first to get a context
 		context = super().get_context_data(**kwargs)
 
@@ -229,4 +263,42 @@ class ProductosMenuStockList(generic.ListView):
 		print(context['seleccion_list'])
 		return context
 
+
+	def get(self, request, *args, **kwargs):
+		print("---GET")
+		#self.object = self.get_object(queryset=Publisher.objects.all())
+		self.productoMenu_uuid = kwargs.get("producto")
+		self.updateSelected = request.GET.get("update")
+		print("update ?")
+		print(self.updateSelected)
+		print(self.productoMenu_uuid )
+		#if we are editing selected stock producots
+		if self.updateSelected and 'add' not in request.POST:
+			print("updateSelected TRUE")
+			request.session.flush()
+			selectedProducts_list = ProductosMenuStock.objects.filter(productosMenu_uuid = self.productoMenu_uuid)
+			seleccion = []
+			request.session['seleccion'] = seleccion
+			print(len(selectedProducts_list))
+			print(selectedProducts_list)
+			for prod in selectedProducts_list:
+				print("PROD")
+				print(prod)
+				print(prod.productoStock_uuid.uuid)
+				print(prod.productoStock_uuid.nombre)
+				print(prod.porciones)
+				print("---")
+				productoToAdd = {
+					"obj_uuid": str(prod.uuid),
+					"uuid": str(prod.productoStock_uuid.uuid),
+					"nombre": str(prod.productoStock_uuid.nombre),
+					"qty":str(prod.porciones)
+				} 
+
+				request.session['seleccion'].append(productoToAdd)
+		else:
+			print("updateSelected FALSE")
+			request.session.flush()
+
+		return super().get(request, *args, **kwargs)
 
